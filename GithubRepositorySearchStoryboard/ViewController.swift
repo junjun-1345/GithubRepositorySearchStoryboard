ebf60a84990e85ac63aed596f3aa6c1bd269b00b
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2024-07-08 14:11:07 +0900

    Initial Commit

---
+++ GithubRepositorySearchStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubRepositorySearchStoryboard
+//
+//  Created by 大野純平 on 2024/07/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
